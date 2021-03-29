e226c0da8c3234955999100d9ccbe2aab292cf57
Janusz Schönrock <janusz.schonrock@outlook.com>
2021-03-30 00:56:19 +0200

    Dodano aplikację w React (Kółko i krzyżyk)

---
+++ Laboratorium/Lab4/web-api-dev/mybackend/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/hello", (req, res) => {
+    res.send("Hello World!");
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Nasluchiwanie na porcie! Test ${PORT}`);
+});
