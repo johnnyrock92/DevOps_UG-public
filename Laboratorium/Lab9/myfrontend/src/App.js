f06b3659356da5df90323f17be82294d20912925
Janusz Schönrock <janusz.schonrock@outlook.com>
2021-06-03 21:58:49 +0200

    Dodano frontend

---
+++ Laboratorium/Lab9/myfrontend/src/App.js
@@ -0,0 +1,57 @@
+// import './App.css';
+import {useState} from "react";
+import ListRecipes from './components/ListRecipes';
+import GetRecipe from './components/GetRecipe';
+import AddRecipe from './components/AddRecipe';
+import EditRecipe from './components/EditRecipe';
+
+function App() {
+
+  const [id, setId] = useState(0);
+  const [idEdit, setIdEdit] = useState([]);
+
+  const Recipe = (props) => {
+    return <GetRecipe id={props.id}/>;
+  }
+
+  const ERecipe = (props) => {
+    return <EditRecipe id={props.id} />;
+  }
+
+  return (
+    <div className="container-fluid">
+    <h1>React - Frontend</h1>
+      <div className="row">
+        <div className="col-7">
+          <div className="card">
+            <div className="card-body">
+              <ListRecipes changeParentHandler={setId} changeParentHandlerEdit={setIdEdit}/>
+            </div>
+          </div>
+        </div>
+        <div className="col-5">
+          <div className="card bg-primary text-white">
+            <div className="card-body">
+              <Recipe id={id} />
+            </div>
+          </div>
+          <br/>
+          <div className="card bg-warning">
+            <div className="card-body">
+              <ERecipe id={idEdit}/>
+            </div>
+          </div>
+          <br/>
+          <div className="card bg-success text-white">
+            <div className="card-body">
+              <AddRecipe />
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ Laboratorium/Lab9/myfrontend/src/components/EditRecipe.js
@@ -0,0 +1,64 @@
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+
+const EditRecipe = (props) => {
+
+    const [nazwa, setNazwa] = useState("");
+    const [skladniki, setSkladniki] = useState("");
+    const [opis, setOpis] = useState("");
+    
+    useEffect(() => {
+        axios.get(`api/przepisy/${props.id}`)
+            .then(response => {
+                setNazwa(response.data.nazwa);
+                setSkladniki(response.data.skladniki);
+                setOpis(response.data.opis);
+            })
+            .catch(error => console.log(error));
+    }, [props.id]);
+
+    const handleSubmit = (event) => {
+        console.log(`Dane do aktualizacji ${props.id} ${nazwa} ${skladniki} ${opis}`);
+
+        axios.put(`api/przepisy/edytuj/${props.id}`, {
+            nazwa: nazwa,
+            skladniki: skladniki,
+            opis: opis
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+        // event.preventDefault();
+    }
+
+
+    return (
+        <>
+        <h5 className="card-title">Edytuj przepis kulinarny (PUT)</h5>
+        <h6 className="card-subtitle mb-2 text-muted">ID: {props.id}</h6>
+        <form>
+                <div className="mb-3">
+                    <label className="form-label">Nazwa:</label>
+                    <input type="text" className="form-control" placeholder='Nazwa' value={nazwa} onChange={event => setNazwa(event.target.value)}/>
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Składniki</label>
+                    <textarea type="text" className="form-control" placeholder='Skladniki' value={skladniki} onChange={event => setSkladniki(event.target.value)} rows="3"></textarea>
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Opis</label>
+                    <textarea className="form-control" placeholder='Opis' value={opis} onChange={event => setOpis(event.target.value)} rows="3"></textarea>
+                </div>
+                
+                <input type='submit' className="btn btn-secondary" value='Aktualizuj przepis' onClick={handleSubmit} />
+                
+            </form>
+        </>
+    );
+}
+
+export default EditRecipe;
